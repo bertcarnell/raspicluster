526398f9862a388588212057553034c3143f91f4
Rob <bertcarnell@gmail.com>
2018-04-10 20:46:39 -0400

    Update practice_methods.R

--- docs/parallel_R/practice_methods.R
+++ docs/parallel_R/practice_methods.R
@@ -3,14 +3,16 @@
 #   install.packages("Rmpi", repos="http://cran.stat.ucla.edu", configure.args="--with-mpi=/usr/lib/arm-linux-gnueabihf/openmpi")
 #   install.packages("snow", repos="http://cran.stat.ucla.edu")
 # ssh into all nodes to set the fingerprint
-
-#
-# Work on the hostname file for openMPI
+#   sudo nano /etc/openmpi/openmpi-default-hostfile
+# Add N01, N02, ..., N05 on separate lines
 #
 
 require(snow)
+require(Rmpi)
+cl <- makeMPIcluster(5)
+
+
 nodes <- paste0("N0", 1:5)
-cl <- makeMPIcluster()
 setDefaultClusterOptions("homogeneous"=FALSE, type="SOCK")
 pinode <- list(host="N01", 
                        rscript="/usr/local/lib/R/bin/Rscript", 
